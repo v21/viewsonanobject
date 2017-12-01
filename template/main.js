8b1eb99fce5bc54c2dd4188a3f676b64d1f4c31d
George Buckenham <vtwentyone@gmail.com>
2017-12-01 16:49:40 -0500

    do obj fuckery within the page js

--- template/main.js
+++ template/main.js
@@ -83,145 +83,203 @@ var render = function(params)
 	directionalLight.position.set( 1, 1, 1 ).normalize();
 	scene.add( directionalLight );
 
-
 	// instantiate a loader
 	var loader = new THREE.OBJLoader();
-	var object;
-	// load a resource
-	loader.load(
-		// resource URL
-		filename,
-		// called when resource is loaded
 
-		
+	function renderObject(object)
+	{
 
-		function ( object ) {
-			object.frustumCulled = false;
 
-			if (params.showSingleGroup)
-			{
-				console.log("group : " + params.group)
-				for (var i = 0; i < object.children.length; i++) {
-					object.children[i].visible = (i == params.group);
-				}
+		object.frustumCulled = false;
+
+		if (params.showSingleGroup)
+		{
+			console.log("group : " + params.group)
+			for (var i = 0; i < object.children.length; i++) {
+				object.children[i].visible = (i == params.group);
 			}
+		}
 
-			var count = 1;
-			if (params.count)
-			{
-				count = params.count;
-			}
+		var count = 1;
+		if (params.count)
+		{
+			count = params.count;
+		}
 
-			var objects = [];
+		var objects = [];
 
-			for (var i = 0; i < count; i++) {
-				var object = object.clone();
-				objects[i] = object;
-
-				var sum_bbox = null;
-
-				var material = new THREE.MeshLambertMaterial({color: 0xffffff, side: THREE.DoubleSide});
-				object.traverse( function ( child ) {
-			        if ( child instanceof THREE.Mesh ) {
-			        	child.frustumCulled = false;
-			        	
-			            child.material = material;
-
-			            child.geometry.computeBoundingBox();
-			            var bbox = child.geometry.boundingBox;
-
-			            if (!isNaN(bbox.min.x) && !isNaN(bbox.min.y) && !isNaN(bbox.min.z) && !isNaN(bbox.max.x) && !isNaN(bbox.max.y) && !isNaN(bbox.max.z))
-			            {
-			            	if (sum_bbox == null) sum_bbox = bbox;
-			            	else sum_bbox.expandByObject(child);
-			            }
-
-			        }
-			    } );
-
-
-
-				if (sum_bbox == null)
-				{
-					console.error("sum_bbox is null");
-				}
-				else
-				{
-					var center = sum_bbox.getCenter().clone();
-					center.multiplyScalar(-1);
-
-					object.traverse(function (child) {
-		                if (child instanceof THREE.Mesh) {
-		                    child.geometry.translate(center.x, center.y, center.z);
-		                }
-		            });
-				}
-
-
-
-				scene.add( object );
-
-				if (params.randomRot)
-				{
-					object.rotation.set(rng.random(0.0, 2.0 * Math.PI), rng.random(0.0, 2.0 * Math.PI), rng.random(0.0, 2.0 * Math.PI));
-				}
-
-			}
+		for (var i = 0; i < count; i++) {
+			var object = object.clone();
+			objects[i] = object;
 
 			var sum_bbox = null;
 
-			scene.updateMatrixWorld();	
-			
-			for (var i = 0; i < objects.length; i++) {
-				
-				objects[i].traverse( function ( child ) {
-			        if ( child instanceof THREE.Mesh ) {
+			var material = new THREE.MeshLambertMaterial({color: 0xffffff, side: THREE.DoubleSide});
+			object.traverse( function ( child ) {
+		        if ( child instanceof THREE.Mesh ) {
+		        	child.frustumCulled = false;
+		        	
+		            child.material = material;
 
-			            child.geometry.computeBoundingBox();
-			            var bbox = child.geometry.boundingBox;
+		            child.geometry.computeBoundingBox();
+		            var bbox = child.geometry.boundingBox;
 
-			            if (!isNaN(bbox.min.x) && !isNaN(bbox.min.y) && !isNaN(bbox.min.z) && !isNaN(bbox.max.x) && !isNaN(bbox.max.y) && !isNaN(bbox.max.z))
-			            {
-			            	if (sum_bbox == null) sum_bbox = bbox;
-			            	else sum_bbox.expandByObject(child);
-			            }
+		            if (!isNaN(bbox.min.x) && !isNaN(bbox.min.y) && !isNaN(bbox.min.z) && !isNaN(bbox.max.x) && !isNaN(bbox.max.y) && !isNaN(bbox.max.z))
+		            {
+		            	if (sum_bbox == null) sum_bbox = bbox;
+		            	else sum_bbox.expandByObject(child);
+		            }
 
-			        }
-			    } );
-			}
+		        }
+		    } );
 
-			console.log(sum_bbox);
+
+
 			if (sum_bbox == null)
 			{
 				console.error("sum_bbox is null");
 			}
-
-			console.log(aspect);
-			var size = sum_bbox.getSize();
-			var cameraSize = Math.max(size.x, size.y) * 1.2;
-			camera.left = cameraSize * aspect / - 2;
-			camera.right = cameraSize * aspect / 2;
-			camera.top = cameraSize / 2;
-			camera.bottom = cameraSize / - 2;
-			if (params.slices)
+			else
 			{
-				console.log("slicing");
-				var sliceWidth = size.z / (params.sliceCount);
-				camera.far = -size.z / 2 + params.sliceIndex * sliceWidth;
-				camera.near = -size.z / 2 + (params.sliceIndex + 1) * sliceWidth;
-				//camera.
+				var center = sum_bbox.getCenter().clone();
+				center.multiplyScalar(-1);
+
+				object.traverse(function (child) {
+	                if (child instanceof THREE.Mesh) {
+	                    child.geometry.translate(center.x, center.y, center.z);
+	                }
+	            });
 			}
-			camera.updateProjectionMatrix();
 
-			console.log(center);
-		
-		
-			renderer.render(scene, camera);
 
-			context.drawImage( renderer.domElement, 0, 0 ,width, height);
+
+			scene.add( object );
+
+			if (params.randomRot)
+			{
+				object.rotation.set(rng.random(0.0, 2.0 * Math.PI), rng.random(0.0, 2.0 * Math.PI), rng.random(0.0, 2.0 * Math.PI));
+			}
+
 		}
-		);
 
+		var sum_bbox = null;
+
+		scene.updateMatrixWorld();	
+		
+		for (var i = 0; i < objects.length; i++) {
+			
+			objects[i].traverse( function ( child ) {
+		        if ( child instanceof THREE.Mesh ) {
+
+		            child.geometry.computeBoundingBox();
+		            var bbox = child.geometry.boundingBox;
+
+		            if (!isNaN(bbox.min.x) && !isNaN(bbox.min.y) && !isNaN(bbox.min.z) && !isNaN(bbox.max.x) && !isNaN(bbox.max.y) && !isNaN(bbox.max.z))
+		            {
+		            	if (sum_bbox == null) sum_bbox = bbox;
+		            	else sum_bbox.expandByObject(child);
+		            }
+
+		        }
+		    } );
+		}
+
+		console.log(sum_bbox);
+		if (sum_bbox == null)
+		{
+			console.error("sum_bbox is null");
+			return false;
+		}
+		if (isNaN(sum_bbox.max.x) || sum_bbox.max.x == Number.POSITIVE_INFINITY || sum_bbox.max.x == Number.NEGATIVE_INFINITY)
+		{
+			console.error("sum_bbox is null");
+			return false;
+		}
+
+		console.log(aspect);
+		var size = sum_bbox.getSize();
+		var cameraSize = Math.max(size.x, size.y) * 1.2;
+		camera.left = cameraSize * aspect / - 2;
+		camera.right = cameraSize * aspect / 2;
+		camera.top = cameraSize / 2;
+		camera.bottom = cameraSize / - 2;
+		if (params.slices)
+		{
+			console.log("slicing");
+			var sliceWidth = size.z / (params.sliceCount);
+			camera.far = -size.z / 2 + params.sliceIndex * sliceWidth;
+			camera.near = -size.z / 2 + (params.sliceIndex + 1) * sliceWidth;
+			//camera.
+		}
+		camera.updateProjectionMatrix();
+
+		console.log(center);
+	
+	
+		renderer.render(scene, camera);
+
+		context.drawImage( renderer.domElement, 0, 0 ,width, height);
+		return true;
+	}
+
+	function onLoadModel () {
+
+		
+		for (var i = 0; i < 100; i++) {
+			var text = this.responseText;
+
+
+			if (params.process != null)
+			{
+				var lines = text.split(/\r?\n/);
+				if (params.process == "perturb")
+				{
+					for (x=0; x<lines.length; x++){
+						lines[x] = perturb(lines[x], x); //randomize individual points
+					}
+				}
+				else if (params.process == "sort")
+				{
+					lines.sort();
+				}
+				else if (params.process == "shuffle")
+				{
+					var lines = shuffle(lines); //shuffle lines randomly
+				}
+				else if (params.process == "sometimes fail")
+				{
+					if (Math.random() < 0.8)
+					{
+						lines = [];
+					}
+				}
+
+				text = lines.join("\r\n");
+			}
+
+
+			var object = loader.parse(text);
+			var rendered = renderObject(object);
+
+			console.log(i + "did object render? " + rendered);
+			if (rendered)
+			{
+				break;
+			}
+		}
+
+
+
+
+
+	
+	}
+
+
+	var oReq = new XMLHttpRequest();
+	oReq.addEventListener("load", onLoadModel);
+	oReq.open("GET", filename);
+	oReq.send();
 
 
 }
@@ -238,6 +296,38 @@ function addHtml(params) {
 	header.innerHTML = params.header;
 	header.className = "pageheader";
 	document.getElementById('renders').appendChild( header);
+}
+
+
+function perturb(line, row){
+	line = line.split(" ");
+
+	for (d=0; d<line.length; d++){
+
+	if (line[d].match(/[-]?(\d*\.)?\d+/g)){
+	//line[d] = Number(line[d]) + Math.random()*d*(row*0.001); 
+	line[d] = Number(line[d]) + (row*0.001); 
+	}
+	}
+	line = line.join(" ");
+	return line;
+}
+
+
+function shuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	while (0 !== currentIndex) {
+
+	randomIndex = Math.floor(Math.random() * currentIndex);
+	currentIndex -= 1;
+
+	temporaryValue = array[currentIndex];
+	array[currentIndex] = array[randomIndex];
+	array[randomIndex] = temporaryValue;
+	}
+
+	return array;
 }
 
 
